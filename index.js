da68d4400c65cc89671e78b942b8aade2a68ef4d
Sruthi R <Sruthi.R@mrcooper.com>
2020-12-03 15:01:47 +0530

    Some improvements to the design

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const outputMsg = document.querySelector("#outputTxt");
 
 translateButton.addEventListener("click",function(){
     var input = inputField.value;
+    if(input===""){
+        alert("Type something to translate");
+        return;
+    }
     fetch(generateStringUrl(input))
     .then(response=>response.json())
     .then(json=>outputMsg.innerText=json.contents.translated)
